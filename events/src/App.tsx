00c6e4217e3dab237c7718311e2dcc07fb52bbcc
Arafahlawal24 <arafahlawal24@gmail.com>
2024-02-07 13:32:30 +0100

    Reinitialized frontend with Vite

---
+++ events/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome To Event Arafah</h1>
+      <div className="card">
+        Book an Event
+        
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
